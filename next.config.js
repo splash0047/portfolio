b15202f60f5f2eee66fccd126999baa1b09592c0
splash0047 <140643753+splash0047@users.noreply.github.com>
2025-02-05 15:07:02 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,28 @@
-const path = require('path')
- 
+const path = require("path");
+
 module.exports = {
+  basePath: "/portfolio", // Add this line for GitHub Pages
+  assetPrefix: "/portfolio/", // Add this line for GitHub Pages
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        pathname: '**',
+        protocol: "https",
+        hostname: "media2.dev.to",
+        pathname: "**",
       },
     ],
   },
-}
+};
